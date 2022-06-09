9ed147d36a270bcc1cea26cdd0dd9845a2d88e3f
Jossextreme <josuarez.93@hotmail.com>
2022-06-08 19:17:43 -0600

    Casi Final

---
+++ src/tarea1programacionclienteservidor/Usuario.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea1programacionclienteservidor;
+
+/**
+ *
+ * @author Joselyn Suarez
+ */
+public class Usuario {
+    private String Nombre;
+    private String Apellidos;
+    private int Edad;
+    private String Profesion;
+    
+    //Constructores
+public Usuario () {}
+    
+public Usuario (String Nombre, String Apellidos, int Edad) {
+    this.Nombre = Nombre;
+    this.Apellidos =Apellidos;
+    this.Edad = Edad;
+}
+
+//Metodo
+
+public String getNombre () { return Nombre;  }
+
+    public String getApellidos () { return Apellidos;  }
+
+    public int getEdad () { return Edad;   }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;}
+}
+
+public void doSomething () { System.out.println("Soy Persona");   }        
+
